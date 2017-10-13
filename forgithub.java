4a4ccac50c31e4d5bead67fcbc9ee2808d7112ee
Yeeeeeeeeeeee <brynespe9613@granitesd.org>
2017-10-13 08:08:01 -0600

    Add files via upload

---
+++ forgithub.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class ScannerTest{
+public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("First name please");
+	String name = sc.nextString();
+	Scanner sc = new Scanner (System.in);
+    System.out.println("Birthdate?");
+    int birthday = sc.nextInt();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How long would you like to live?");
+    int life = sc.nextInt;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many ounces of fluid do you consume in a day?(Keep in mind that coffee dehydrates you)(But other than that this includes anything even marinara sauce which is so weird to think about but if you drink that stuff it's like a fruit smoothie because tomatoes are a fruit holy crap I just blew my own MIND WHAAAAAT anyways have a nice time finding out how long you're going to live or whatever unless you get hit by a bus or something anyways... )");
+    int ounces = sc.nextInt;
+    tankerTruck = int(1024000);
+    totalOunces = (life)*365 * (ounces);
+    totalTanker = (totalOunces)/(totalTanker);
+    System.out.println totalTanker;
+    System.out.println("Have a nice life! Don't get hit by a bus or anything! If you got a concerning result maybe talk to a doctor or don't I'm not your parental unit. Anyways I have to go freak out a dude named Dave lol. Name's Hal by the way, it was crazy chill to meet you.");
+}
+
+
+
+}
